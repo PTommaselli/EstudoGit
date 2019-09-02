97b35d6b5dfef6a040483db0b22935e6283a1e38
pedrotommaselli@gmail.com <pedrotommaselli@gmail.com>
2019-09-02 14:25:36 -0400

    chore: alterando retorno de rotas

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send("Hello World"));
+app.get('/', (req, res) => res.send("Hello World New"));
 
 app.listen(2000);
